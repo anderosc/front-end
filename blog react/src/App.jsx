a5024ce327ddafe1bf75e3686786b7b0ace58f97
gimmi74 <andero.sc@gmail.com>
2024-12-21 01:14:11 +0200

    CSS

---
+++ blog react/src/App.jsx
@@ -0,0 +1,15 @@
+
+import TopBar from "./Topbar/topbar"; 
+
+function App() {
+  
+
+  return (
+    <>
+     <TopBar/>
+     <p> </p>
+    </>
+  )
+}
+
+export default App
---
+++ blog react/src/Topbar/TopBar.jsx
@@ -0,0 +1,12 @@
+import "./topbar.css"
+
+export default function TopBar() {
+  return (
+    <div className="top">
+        <div className= "TopLeft" >  left </div>
+        <i class="fa-brands fa-facebook" /> 
+        <div className= "topCenter" >  center</div>
+        <div className= "TopRight" > right  </div>
+        </div>
+  )
+}
