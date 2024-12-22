22d630c5e8b5d7b34c79ef4cfcba96f6377a0b79
gimmi74 <andero.sc@gmail.com>
2024-12-22 18:30:58 +0200

    CSS + HTML
    Topbar

--- blog react/src/Topbar/TopBar.jsx
+++ blog react/src/Topbar/TopBar.jsx
@@ -2,11 +2,29 @@ import "./topbar.css"
 
 export default function TopBar() {
   return (
+
+    // Ignore the errors from lines 8-10
     <div className="top">
-        <div className= "TopLeft" >  left </div>
-        <i class="fa-brands fa-facebook" /> 
-        <div className= "topCenter" >  center</div>
-        <div className= "TopRight" > right  </div>
+      <i class="topIcon fa-brands fa-facebook"></i>
+      <i class="topIcon fa-brands fa-twitter"></i>
+      <i class="topIcon fa-brands fa-instagram"></i>
+        <div className= "TopLeft" >   </div>
+ 
+        <div className= "TopCenter" >  </div>
+        <ul className="topList">
+        <li className="topListItem">HOME</li>
+        <li className="topListItem">ABOUT</li>
+        <li className="topListItem">CONTACT</li>
+        <li className="topListItem">WRITE</li>
+        <li className="topListItem">LOGOUT</li>
+        </ul>
+
+
+
+        <div className= "TopRight" >   </div>
+        
+        <img className="topImg" src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <i class="topSearchIcon fa-solid fa-magnifying-glass"></i>
         </div>
   )
 }
