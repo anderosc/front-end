c208a003d148c649ec4394d3bb2baae689bedb2f
gimmi74 <andero.sc@gmail.com>
2024-12-23 02:09:30 +0200

    New repo

---
+++ kodutoo1/src/App.jsx
@@ -0,0 +1,62 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button className='nupp'> NUPP </button>
+      <p className='tekst'> Tekst bla bla </p>
+      <p className='tekst2'> Teine tekst bla bla</p>
+      <img className='pilt'  src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+    
+
+      <h2>HTML Table</h2>
+
+<table className='tabel'>
+<tr className='tr'>
+  <th className='th'>Company</th>
+  <th className='th'>Contact</th>
+  <th className='th'>Country</th>
+</tr>
+<tr className='tr'>
+  <td className='td'>Alfreds Futterkiste</td>
+  <td className='td'>Maria Anders</td>
+  <td className='td'>Germany</td>
+</tr>
+<tr className='tr'>
+  <td className='td'>Centro comercial Moctezuma</td>
+  <td className='td'>Francisco Chang</td>
+  <td className='td'>Mexico</td>
+</tr>
+<tr className='tr'>
+  <td className='td'>Ernst Handel</td>
+  <td className='td'>Roland Mendel</td>
+  <td className='td'>Austria</td>
+</tr>
+<tr className='tr'>
+  <td className='td'>Island Trading</td>
+  <td className='td'>Helen Bennett</td>
+  <td className='td'>UK</td>
+</tr>
+<tr className='tr'>
+  <td className='td'>Laughing Bacchus Winecellars</td>
+  <td className='td'>Yoshi Tannamuri</td>
+  <td className='td'>Canada</td>
+</tr>
+<tr className='tr'>
+  <td className='td'>Magazzini Alimentari Riuniti</td>
+  <td className='td'>Giovanni Rovelli</td>
+  <td className='td'>Italy</td>
+</tr>
+
+ </table>
+    
+
+ <iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+ </iframe>
+    </>
+  )
+}
+
+export default App
