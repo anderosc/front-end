2b8d980538f08453eb4baa4168ae1a5a64e4b028
gimmi74 <andero.sc@gmail.com>
2024-12-23 22:14:11 +0200

    News

--- news/src/App.jsx
+++ news/src/App.jsx
@@ -1,12 +1,20 @@
 
 import './App.css'
 import {Link, Route, Routes} from 'react-router-dom'
+import Avaleht from './pages/Avaleht'
+import Kontakt from './pages/Kontakt'
+import Meist from './pages/Meist'
+import Uudised from './pages/Uudised'
+
 
 function App() {
   
 
   return (
-    <><div> 
+     
+    <>
+    <div className='Container' >
+    <div className='TopBar'> 
     <Link to='/'>
     <button> Avalehele</button>
     </Link>
@@ -23,16 +31,15 @@ function App() {
     <button> Meist</button>
     </Link>
 
-    <button className='nupp'> TESTi</button>
-    <button> test</button>
+    </div>
     </div>
 
 
      <Routes> 
-      <Route path='' element={<div> See on avaleht</div>} />
-      <Route path='uudised' element={<div> See on uudised</div>} /> 
-      <Route path='kontakt' element={<div> See on kontakt</div> } /> 
-      <Route path='meist' element={<div> See on meist</div>} /> 
+      <Route path='' element={ <Avaleht />} />
+      <Route path='uudised' element={<Uudised />} /> 
+      <Route path='kontakt' element={<Kontakt />} /> 
+      <Route path='meist' element={<Meist />} /> 
 
 
      </Routes>
---
+++ news/src/pages/Uudised.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Uudised() {
+  return (
+    <div>  
+
+        
+    </div>
+  )
+}
+
+export default Uudised
