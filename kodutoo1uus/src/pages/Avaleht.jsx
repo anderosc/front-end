e2962590f234824cf942f4dbe9ecd0e3715b489e
gimmi74 <andero.sc@gmail.com>
2024-12-31 05:02:00 +0200

    Login/log out

--- kodutoo1uus/src/pages/Avaleht.jsx
+++ kodutoo1uus/src/pages/Avaleht.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function Avaleht() {
+  const [sisselogitud, muudaSisselogitud] = useState("ei")
+  // const [loginout, muudaloginout] = useState("Logi sisse")
+const kasutajaNimiRef = useRef();
+  const paroolRef = useRef();
+
+  const[sonum, muudaSonum] = useState("")
+
+    const logiSisse = () => {
+      if(paroolRef.current.value === "123"){
+        muudaSisselogitud("jah")
+        muudaSonum("Tere tulemast, " + kasutajaNimiRef.current.value)
+      } else {
+        muudaSonum("Vale parool")
+      }
+    }
+
+    const logiValja = () => {
+      muudaSisselogitud("ei");
+      muudaSonum("");
+    }
+  
   return (
+    <> 
     <div>Avaleht</div>
+    <br />
+    <br />
+    <br />
+    <div className='App'> 
+
+      <div>{sonum}</div>
+    
+        {sisselogitud === "ei" && <div>
+      <div>Kasutajanimi</div>
+      <input ref={kasutajaNimiRef} type="text" /> <br />
+      <div>Parool</div>
+      <input ref={paroolRef} type="password" />
+      </div> }
+
+
+    {sisselogitud === "ei" && <button onClick={logiSisse}>Logi sisse</button>}
+    {sisselogitud === "jah" && <button onClick={logiValja}>Logi Välja</button>}
+
+    </div>
+
+    </>
   )
 }
 
--- kodutoo1uus/src/pages/Kontakt.jsx
+++ kodutoo1uus/src/pages/Kontakt.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import  { useState } from 'react'
 
 function Kontakt() {
+
+  const [aadress, määraAadress] = useState ("Tallinn");
+  const [telefon, määraTelefon] = useState ("54 54 10 10");
+  const [email, määraEmail] = useState ("bla@bla.com ");
+  const [ingliseKeelne, määraIngliseKeelne] = useState ("ei");
+
+  const ingliseks = () =>  {
+    määraAadress("Lodon")
+    määraTelefon("54 55 40 40")
+    määraEmail("blahhhh@gmallll.com")
+    määraIngliseKeelne("jah")
+    }
+
+
   return (
-    <div>Kontakt</div>
+    <> 
+    <div>{aadress}</div>
+    <div>{telefon}</div>
+    <div> {email} </div>
+
+    <button onClick={ingliseks}> Muudai inglis keelde </button>
+    { ingliseKeelne === "jah" && <div> Leht on inglise keelne</div>}
+    </>
   )
 }
 
