1a8d6c89e5c750a2c1734b98d058ce512f599bd1
gimmi74 <andero.sc@gmail.com>
2024-12-31 06:14:31 +0200

    small changes

--- kodutoo1uus/src/App.jsx
+++ kodutoo1uus/src/App.jsx
@@ -1,6 +1,7 @@
 import Kontakt from "./pages/Kontakt"
 import Avaleht from "./pages/Avaleht"; 
 import Meist from "./pages/Meist";
+import Input from "./pages/input";
 import { Link, Routes, Route } from 'react-router-dom';
 
 import './App.css'
@@ -21,12 +22,17 @@ function App() {
       <button> Kontakt</button>
       </Link>
 
+      <Link to="/input">
+      <button> Input </button>
+      </Link>
+
       
      
       <Routes>
         <Route path="/" element={< Avaleht />} />
         <Route path="/kontakt" element={<Kontakt />} />
         <Route path="/meist" element={<Meist />} />
+        <Route path="/input" element={<Input />} />
 
 
       </Routes>
