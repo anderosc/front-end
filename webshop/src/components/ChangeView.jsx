4984b48a09d91b3bfa53c9129ae383add8d63dc6
Andero <andero.sc@gmail.com>
2025-03-04 14:14:09 +0200

    Delete webshop directory

--- webshop/src/components/ChangeView.jsx
+++
@@ -1,9 +0,0 @@
-import { useMap } from 'react-leaflet';
-
-function ChangeView(props) {  
-  const map = useMap();  
-  map.setView(props.center, props.zoom);  
-  return null;
-}
-
-export default ChangeView;
